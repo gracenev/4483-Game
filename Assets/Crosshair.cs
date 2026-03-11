ad880f60eba020f9921b226b2aa015340fbe76e5
harshikamboj <harshi.s.kamboj@gmail.com>
2026-03-11 14:27:04 -0400

    Updated Crosshair.cs
    Made it so the crosshair is dynamic, get's bigger when it is closer to an interactive item.

--- Assets/Crosshair.cs
+++ Assets/Crosshair.cs
@@ -2,31 +2,39 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    public int dotSize = 6;
+    public float dotSize = 4f;
+    public float hoverDotSize = 10f;
+    public float interactRange = 3f;
     private Texture2D dotTexture;
+    private bool hovering = false;
 
     void Start()
     {
-        dotTexture = new Texture2D(dotSize, dotSize);
-        float radius = dotSize / 2f;
-        Color clear = new Color(0, 0, 0, 0);
-
-        for (int x = 0; x < dotSize; x++)
-        {
-            for (int y = 0; y < dotSize; y++)
-            {
-                float dist = Vector2.Distance(new Vector2(x, y), new Vector2(radius, radius));
-                dotTexture.SetPixel(x, y, dist <= radius ? Color.white : clear);
-            }
-        }
-
+        dotTexture = new Texture2D(1, 1);
+        dotTexture.SetPixel(0, 0, Color.white);
         dotTexture.Apply();
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactRange))
+        {
+            hovering = hit.collider.GetComponent<InteractableButton>() != null;
+        }
+        else
+        {
+            hovering = false;
+        }
     }
 
     void OnGUI()
     {
-        float x = Screen.width / 2f - dotSize / 2f;
-        float y = Screen.height / 2f - dotSize / 2f;
-        GUI.DrawTexture(new Rect(x, y, dotSize, dotSize), dotTexture);
+        float size = hovering ? hoverDotSize : dotSize;
+        float x = Screen.width / 2f - size / 2f;
+        float y = Screen.height / 2f - size / 2f;
+        GUI.DrawTexture(new Rect(x, y, size, size), dotTexture);
     }
 }
